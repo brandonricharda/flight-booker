212afd38ddb0417e2d544e0535b0883e1ccf3226
brandonricharda <brandonricharda@gmail.com>
2021-03-17 22:52:22 -0400

    Corrects bugs in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-airports = Airport.create([{ code: "SFO" }, { code: "NYC" }])
+airport_codes = ["SFO", "NYC"]
+
+airport_codes.each { |city| Airport.create(code: city) }
 
 dates = []
 
 1.upto(12) { |hour| dates << DateTime.new(2021, 1, 1, hour, 0) }
 
-airports.each do |airport|
-    destination = airports.select { |x| x != airport }
+Airport.all.each do |airport|
+    destination = Airport.select { |x| x != airport }.first
     dates.each_slice(2) do |start, finish|
         Flight.create(from_airport_id: airport.id, to_airport_id: destination.id, departure: start, duration: finish.hour - start.hour)
     end
