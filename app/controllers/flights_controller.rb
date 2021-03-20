5da3107e6bfe5b73aa633efa8b6a7f9a941e2525
brandonricharda <brandonricharda@gmail.com>
2021-03-20 00:21:02 -0400

    Adds from_airport and to_airport dropdowns to homepage

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,9 +1,6 @@
 class FlightsController < ActionController::Base
     def index
         @flights = Flight.all
-    end
-
-    def new
         @airport_options = Airport.all.map { |airport| [airport.code, airport.id] }
     end
 end
