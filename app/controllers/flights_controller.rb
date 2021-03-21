b49281ccdaf5b8a700c0aebc02e0525bd5d1586e
brandonricharda <brandonricharda@gmail.com>
2021-03-20 23:31:11 -0400

    Adds departure date dropdown to root page

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,6 @@ class FlightsController < ActionController::Base
     def index
         @flights = Flight.all
         @airport_options = Airport.all.map { |airport| [airport.code, airport.id] }
+        @flight_dates = Flight.pluck(:departure).uniq
     end
 end
