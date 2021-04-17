c2e1e150c67ec78c4133dcda71cca7286a747208
brandonricharda <brandonricharda@gmail.com>
2021-04-17 00:42:20 -0400

    Updates so each passenger receives a confirmation email

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -2,7 +2,12 @@ class PassengerMailer < ApplicationMailer
     default from: "brandonricharda@gmail.com"
 
     def welcome_email
-        @passenger = Passenger.find_by(booking_id: params[:booking][:id])
-        mail(:to => @passenger.email, :subject => "Booking Confirmation")
+        @passengers = Passenger.where(booking_id: params[:booking][:id])
+        @emails = @passengers.map { |passenger| passenger.email }
+        @flight = Flight.find_by(id: params[:booking][:flight_id])
+        @from_airport = Airport.find_by(id: @flight.from_airport).code 
+        @to_airport = Airport.find_by(id: @flight.to_airport).code 
+        @date = @flight.departure
+        mail(:to => @emails, :subject => "Booking Confirmation")
     end
 end
